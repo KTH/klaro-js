145894c886b9a760d3b306a34f898180e56054ca
Martina Thege <martinathege@gmail.com>
2023-07-07 11:19:19 +0200

    set up test environment

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const SRC_DIR = path.resolve(__dirname, 'src');
 
+
+new webpack.DefinePlugin({
+    __REACT_DEVTOOLS_GLOBAL_HOOK__: '({ isDisabled: true })',
+});
+
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
@@ -33,6 +38,7 @@ module.exports = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+            'Content-Security-Policy': " style-src 'self'",
         },
         allowedHosts: 'all',
     }
